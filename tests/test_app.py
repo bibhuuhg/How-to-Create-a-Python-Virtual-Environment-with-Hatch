f7639993cfc1d753686284d8faa3d6a7d624ed9f
bibhuuhg <bibhu.giet@gmail.com>
2024-09-11 15:26:53 +0530

    lint tools

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,10 @@
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
-
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual("foo".upper(), "FOO")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
